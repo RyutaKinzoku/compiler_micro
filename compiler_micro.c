779e8cb44d3b26c70a9525035f47d71813bdca82
Kenneth Ibarra Vargas <66222501+RyutaKinzoku@users.noreply.github.com>
2022-02-27 16:53:59 -0600

    Move functions

--- compiler_micro.c
+++ compiler_micro.c
@@ -30,6 +30,7 @@ extern token scanner(void);
 
 extern char token_buffer[64];
 int token_buffer_index = 0;
+extern token current_token;
 
 int compareToken(char a[], int size){
     if(size == token_buffer_index){
@@ -145,7 +146,88 @@ token scanner(void){
 }
 
 void match(token t){
-    
+    token s = scanner();
+    if (t == s)
+    {
+        current_token = t;
+    } else
+    {
+        printf("The token is not allowed.");
+    }
+}
+
+void check_id(string s)
+{
+    if(!lookup(s)){
+        enter(s);
+        generate("Declare", s, "Integer", "");
+    }
+}
+
+char *get_temp(void)
+{
+    static int max_temp = 0;
+    static char tempname[MAXIDLEN];
+
+    max_temp++;
+    sprintf(tempname, "Temp&%d", max_temp);
+    check_id(tempname);
+    return tempname;
+}
+
+void start(void){
+    /*Semantic initializationMAs, none needed. */
+}
+
+void finish(void){
+    generate("Halt", "", "", "");
+}
+
+void assign(expr_rec target, expr_rec source){
+    generate("Store", extract(source), target.name, "");
+}
+
+op_rec process_op(void){
+    op_rec o;
+    if (token_buffer == PLUSOP)
+        o.operator = PLUS;
+    else 
+        o.operator = MINUS;
+    return o;
+}
+
+expr_rec gen_infix(expr_rec e1, op_rec op, expr_rec e2)
+{
+    expr_rec e_rec;
+    e_rec.kind = TEMPEXPR;
+
+    strcopy(e_rec.name, get_temp());
+    generate(extract(op), extract(e1), extract(e2), e_rec.name);
+    return e_rec;
+}
+
+void read_id(expr_rec in_var){
+    generate("Read", in_var.name, "Integer", "");
+}
+
+expr_rec process_id(void){
+    expr_rec t;
+    check_id(token_buffer);
+    t.kind = IDEXPR;
+    strcpy(t.name, token_buffer);
+    return t;
+}
+
+expr_rec process_literal(void){
+    expr_rec t;
+    t.kind = LITERALEXPR;
+    (void) sscanf(token_buffer, "%d", & t.value);
+    return t;
+}
+
+void write_expr(expr_rec out_expr)
+{
+    generate("Write", extract(out_expr), "Integer", "");
 }
 
 void program(void){
@@ -273,78 +355,4 @@ void primary(expr_rec *e_rec){
         syntax_error(tok);
         break;
     }
-}
-
-void check_id(string s)
-{
-    if(!lookup(s)){
-        enter(s);
-        generate("Declare", s, "Integer", "");
-    }
-}
-
-char *get_temp(void)
-{
-    static int max_temp = 0;
-    static char tempname[MAXIDLEN];
-
-    max_temp++;
-    sprintf(tempname, "Temp&%d", max_temp);
-    check_id(tempname);
-    return tempname;
-}
-
-void start(void){
-    /*Semantic initializationMAs, none needed. */
-}
-
-void finish(void){
-    generate("Halt", "", "", "");
-}
-
-void assign(expr_rec target, expr_rec source){
-    generate("Store", extract(source), target.name, "");
-}
-
-op_rec process_op(void){
-    op_rec o;
-    if (token_buffer == PLUSOP)
-        o.operator = PLUS;
-    else 
-        o.operator = MINUS;
-    return o;
-}
-
-expr_rec gen_infix(expr_rec e1, op_rec op, expr_rec e2)
-{
-    expr_rec e_rec;
-    e_rec.kind = TEMPEXPR;
-
-    strcopy(e_rec.name, get_temp());
-    generate(extract(op), extract(e1), extract(e2), e_rec.name);
-    return e_rec;
-}
-
-void read_id(expr_rec in_var){
-    generate("Read", in_var.name, "Integer", "");
-}
-
-expr_rec process_id(void){
-    expr_rec t;
-    check_id(token_buffer);
-    t.kind = IDEXPR;
-    strcpy(t.name, token_buffer);
-    return t;
-}
-
-expr_rec process_literal(void){
-    expr_rec t;
-    t.kind = LITERALEXPR;
-    (void) sscanf(token_buffer, "%d", & t.value);
-    return t;
-}
-
-void write_expr(expr_rec out_expr)
-{
-    generate("Write", extract(out_expr), "Integer", "");
 }
